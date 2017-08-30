9824a8f8d09e155e2acac917fbd6780090cc54a7
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-30 16:39:55 -0400

    ~4pm commit for submission, edit is working, need delete feature

--- app.js
+++ app.js
@@ -9,33 +9,68 @@ const port = 3000;
 const Books = require('./models/books');
 
 const mongoURL = 'mongodb://localhost:27017/books';
-mongoose.connect(mongoURL, {useMongoClient: true});
+mongoose.connect(mongoURL, {
+ useMongoClient: true
+});
 mongoose.Promise = require('bluebird');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
 
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 
-app.use('/static', express.static('static'));
+app.use(express.static(__dirname + '/public'));
 
 // setting up all routes to pages
-app.get('/add', function(req, res){
+app.get('/add', function(req, res) {
  res.render('add');
 });
 
-app.post('/add', function(req, res){
- Books.create(req.body).then(function (books){
+app.post('/add', function(req, res) {
+ Books.create(req.body).then(function(books) {
   res.redirect('/');
  });
 });
 
-app.get('/', function(req, res){
- Books.find().then(function (books){
-  res.render('index', {books: books});
+app.get('/:id/edit', function(req, res) {
+ Books.find({
+  _id: req.params.id
+ }).then(function(books) {
+  res.render('edit', {
+   books: books
+  });
+ });
+});
+
+app.post('/:id/edit', function(req, res) {
+ Books.findOneAndUpdate({
+  _id: req.params.id
+ }, req.body).then(function(books) {
+  res.redirect('/')
+ });
+});
+
+app.get('/:id', function(req, res) {
+ Books.find({
+  _id: req.params.id
+ }).then(function(books) {
+  res.render('id', {
+   books: books
+  });
+ });
+});
+
+
+app.get('/', function(req, res) {
+ Books.find().then(function(books) {
+  res.render('index', {
+   books: books
+  });
  });
 });
 
